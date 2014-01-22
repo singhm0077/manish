7d14079a44b8b550a75b081a5097cb0584360c33
Newcon <smishra@newconinfosystems.com>
2014-01-22 11:19:46 +0530

    shailendr - added new class

---
+++ App_Code/NewClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for NewClass
+/// </summary>
+public class NewClass
+{
+	public NewClass()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
